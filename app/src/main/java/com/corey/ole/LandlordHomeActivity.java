6c9701dba60197cee4e53f34ce54139c7da59659
Autumn Wozniak <autumnwozniak@berkeley.edu>
2019-05-03 22:26:46 -0700

    Landlord home navbar

--- app/src/main/java/com/corey/ole/LandlordHomeActivity.java
+++ app/src/main/java/com/corey/ole/LandlordHomeActivity.java
@@ -1,16 +1,17 @@
 package com.corey.ole;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,33 +21,31 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-/* Activity for Landlord's Home UI */
+public class LandlordHomeActivity extends NavDrawerActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-public class LandlordHomeActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
     Toolbar toolbar;
-    ActionBarDrawerToggle actionBarDrawerToggle;
     PropertyAdapter mAdapter;
     RecyclerView mRecyclerView;
     ArrayList<Property> mProperties = new ArrayList<>();
     ArrayList<String> mPropertyIds;
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.landlord_home);
-        toolbar = findViewById(R.id.landlord_home_toolbar);
-        toolbar.setTitle("My Properties");
-        toolbar.setTitleTextColor(Color.WHITE);
+        setContentView(R.layout.activity_landlord_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawerLayout = findViewById(R.id.landlord_home_drawer);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.drawer_open, R.string.drawer_close);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        setDrawerData(navigationView);
 
-        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
         mRecyclerView = findViewById(R.id.landlord_home_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -85,32 +84,6 @@ public class LandlordHomeActivity extends AppCompatActivity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
-
-
-    }
-    @Override
-    public void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        actionBarDrawerToggle.syncState();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_new) {
-            Intent intent = new Intent(this, EditAddPropertyActivity.class);
-            intent.putExtra("isAdd", true);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.add, menu);
-        return true;
     }
 
     private void setAdapterAndUpdateData() {
@@ -167,5 +140,55 @@ public class LandlordHomeActivity extends AppCompatActivity {
         d4.setValue(p4);
 
         setAdapterAndUpdateData();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_new) {
+            Intent intent = new Intent(this, EditAddPropertyActivity.class);
+            intent.putExtra("isAdd", true);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Do nothing
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/corey/ole/PropertyDetailsActivity.java
+++ app/src/main/java/com/corey/ole/PropertyDetailsActivity.java
@@ -2,8 +2,6 @@ package com.corey.ole;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -13,7 +11,6 @@ import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.Button;
