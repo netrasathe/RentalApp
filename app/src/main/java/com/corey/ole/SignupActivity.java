5ed453a9fcf2dd6859e6cdf42dcc7d5c196e2e6e
coreywho <coreyhu@berkeley.edu>
2019-05-06 22:21:00 -0700

    complete signup layout

---
+++ app/src/main/java/com/corey/ole/SignupActivity.java
@@ -0,0 +1,17 @@
+package com.corey.ole;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar_signup_personal_info);
+
+    }
+
+
+
+}
