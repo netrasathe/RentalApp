fa47235fa7551b84c2722fec689966af9b8ebc6a
netrasathe <netrasathe@berkeley.edu>
2019-05-08 10:14:48 -0700

    Add files via upload

---
+++ app/src/main/java/com/corey/ole/PDF.java
@@ -0,0 +1,21 @@
+public class PDFMain {
+
+    private String n;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return n;
+    }
+
+    public void setName(String name) {
+        this.n = n;
+    }
+}
